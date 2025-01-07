772b6cdedf5b5ec93f09fb7fd4fbc5448c342fce
rodrigoaguilar619 <rodrigoaguilar619@gmail.com>
2025-01-06 23:52:44 -0600

    Adjust load routes to define template on mainWebpack

--- src/appModules/app/appMain.routing.ts
+++ src/appModules/app/appMain.routing.ts
@@ -1,5 +1,4 @@
 import { Routes } from '@angular/router';
-import TemplateComponent from '@app/appComponents/templates/environments/coreui/template/template.component';
 import { SUB_PATHS } from '@app/appModules/catalogs/pathsCatalog';
 import { LoginComponent } from '@app/appComponents/templates/logins/coreui/login.component';
 import { AuthGuard } from '@app/appComponents/instances/authInstances/authGuard';
@@ -8,45 +7,57 @@ import { appAdminRoutes } from './admin/admin.routing';
 import { appFoodRoutes } from './food/food.routing';
 import { appRecipeRoutes } from './recipe/recipe.routing';
 import { appDietRoutes } from './diet/diet.routing';
+import { _APP_ROUTE_START_ } from '@app/appComponents/catalogs/constantCatalog';
 
-const routes: Routes = [{
-  path: '',
-  redirectTo: SUB_PATHS.ADMIN.PRINCIPAL_PAGE.fullPath,
-  pathMatch: "full"
-},
-{
-  path: '',
-  component: TemplateComponent,
-  children: [
-    {
-      path: SUB_PATHS.ADMIN.path,
-      canActivate: [AuthGuard],
-      children: appAdminRoutes
-    },
-    {
-      path: SUB_PATHS.FOOD.path,
-      canActivate: [AuthGuard],
-      children: appFoodRoutes
-    },
-    {
-      path: 'recipe',
-      canActivate: [AuthGuard],
-      children: appRecipeRoutes
-    },
-    {
-      path: 'diet',
-      canActivate: [AuthGuard],
-      children: appDietRoutes
-    },
-  ]
-},
-{
-  path: 'login',
-  component: LoginComponent
-},
-{
-  path: 'logout',
-  component: LogoutLayoutComponent
-}];
+const appMainRoutes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'logout',
+    component: LogoutLayoutComponent
+  }
+];
 
-export const appRoutes: Routes = routes;
+const appModuleRoutes: Routes = [
+  {
+    path: SUB_PATHS.ADMIN.path,
+    canActivate: [AuthGuard],
+    children: appAdminRoutes
+  },
+  {
+    path: SUB_PATHS.FOOD.path,
+    canActivate: [AuthGuard],
+    children: appFoodRoutes
+  },
+  {
+    path: 'recipe',
+    canActivate: [AuthGuard],
+    children: appRecipeRoutes
+  },
+  {
+    path: 'diet',
+    canActivate: [AuthGuard],
+    children: appDietRoutes
+  },
+]
+
+export function getRoutes(tempateComponent: any) {
+
+  const routes: Routes = [{
+    path: '',
+    redirectTo: _APP_ROUTE_START_,
+    pathMatch: "full"
+  },
+  {
+    path: '',
+    component: tempateComponent,
+    children: appModuleRoutes
+  },
+  ...appMainRoutes
+  ];
+
+  return routes;
+
+}
--- src/mainWebpack.ts
+++ src/mainWebpack.ts
@@ -4,15 +4,17 @@ import './scss/environments/coreui/stylesApp.scss';
 
 import { bootstrapApplication } from '@angular/platform-browser';
 import { getLayoutConfig } from './appComponents/templates/environments/coreui/layout.config';
+import TemplateComponent from './appComponents/templates/environments/coreui/template/template.component';
 
 /*import AppMainComponent from './_moduleTest/app/appTest.component';
 import { appRoutes } from './_moduleTest/app/appTest.routing';*/
 import AppMainComponent from './appModules/app/appMain.component';
-import { appRoutes } from './appModules/app/appMain.routing';
+import { getRoutes } from './appModules/app/appMain.routing';
 
+const template = TemplateComponent;
 export const customReducers = {};
 
-bootstrapApplication(AppMainComponent, getLayoutConfig(appRoutes, customReducers)).catch((err) => console.error(err));
+bootstrapApplication(AppMainComponent, getLayoutConfig(getRoutes(template), customReducers)).catch((err) => console.error(err));
 
 if (module['hot']) {
   module['hot'].accept();
