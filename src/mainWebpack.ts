f627726320abb01b5671f4733a1235ec4d0f2675
rodrigoaguilar619 <rodrigoaguilar619@gmail.com>
2025-01-07 22:41:52 -0600

    Set layout config global to avoid set it on every template

--- src/mainWebpack.ts
+++ src/mainWebpack.ts
@@ -1,17 +1,16 @@
 import '@angular/compiler';
 import { bootstrapApplication } from '@angular/platform-browser';
+import { getLayoutConfig } from './main.config';
 
 //-------- TEMPLATE COREUI CONFIG ----------------
 import './scss/environments/coreui/styles.scss';
-import './scss/environments/coreui/stylesApp.scss';
-import { getLayoutConfig } from './appComponents/templates/environments/coreui/layout.config';
+import './scss/environments/coreui/stylesApp.scss'
 import TemplateComponent from './appComponents/templates/environments/coreui/template/template.component';
 
 //-------- TEMPLATE PRIMENG CONFIG ----------------
 /*import './scss/environments/primeng/styles.scss';
 import './scss/environments/primeng/stylesApp.scss';
-import TemplateComponent from './appComponents/templates/environments/primeng/template/template.component';
-import { getLayoutConfig } from './appComponents/templates/environments/primeng/layout.config';*/
+import TemplateComponent from './appComponents/templates/environments/primeng/template/template.component';*/
 
 //-------- APP TEST CONFIG ----------------
 /*import AppMainComponent from './_moduleTest/app/appTest.component';
