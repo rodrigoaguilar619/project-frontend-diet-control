8b47b9214816109e7651cf0c446309367d548e8d
rodrigoaguilar619 <rodrigoaguilar619@gmail.com>
2025-01-26 11:58:27 -0600

    Adjust routes for test modules

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,18 @@
 import '@angular/compiler';
+
+//-------- TEMPLATE COREUI CONFIG ----------------
 import './scss/environments/coreui/styles.scss';
-import './scss/environments/coreui/stylesApp.scss';
+import './scss/environments/coreui/stylesApp.scss'
+import TemplateComponent from './appComponents/templates/environments/coreui/template/template.component';
 
 import { bootstrapApplication } from '@angular/platform-browser';
 import AppMainComponent from './_moduleTest/app/appTest.component';
 
-import { appRoutes } from './_moduleTest/app/appTest.routing';
 import { getLayoutConfig } from './main.config';
+import { getRoutes } from './_moduleTest/app/appTest.routing';
 
+
+const template = TemplateComponent;
 export const customReducers = {};
 
-bootstrapApplication(AppMainComponent, getLayoutConfig(appRoutes, customReducers)).catch((err) => console.error(err));
+bootstrapApplication(AppMainComponent, getLayoutConfig(getRoutes(template), customReducers)).catch((err) => console.error(err));
