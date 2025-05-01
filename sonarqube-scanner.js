8518a26acc0fd14982cd72f8ef05cfd53ef57b9e
rodrigoaguilar619 <rodrigoaguilar619@gmail.com>
2025-05-01 11:58:05 -0600

    Updated sonarqube config

--- sonarqube-scanner.js
+++ sonarqube-scanner.js
@@ -1,4 +1,4 @@
-const scanner = require('sonarqube-scanner');
+/*const scanner = require('sonarqube-scanner');
 const { program } = require('commander');
 program
   .option('-t, --token <token>', 'SonarQube token')
@@ -15,5 +15,34 @@ const scannerConfig = {
       "sonar.exclusions": "src/polyfills.ts"
     },
   };
-  
+
+scanner.scan(scannerConfig, () => process.exit());*/
+
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+
+const scanner = require('sonarqube-scanner');
+import { program } from 'commander';
+
+program
+  .option('-t, --token <token>', 'SonarQube token')
+  .option('-h, --host <host>', 'SonarQube server URL')
+  .parse(process.argv);
+
+console.log("test params command: " + JSON.stringify(program.opts()));
+
+const options = program.opts();
+console.log("Token:", options.token);
+console.log("Host:", options.host);
+
+const scannerConfig = {
+  serverUrl: options.host,
+  token: options.token,
+  options: {
+    "sonar.sources": "./src",
+    "sonar.coverage.exclusions": "src/**",
+    "sonar.exclusions": "src/polyfills.ts",
+  },
+};
+
 scanner.scan(scannerConfig, () => process.exit());
